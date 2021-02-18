566717ca2d2bb8999ddd3a1cab6f7fc5883fe1fe
ueabu <ueabu@iastate.edu>
2021-02-18 00:41:19 -0600

    Added the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
+import umacodes from './assets/images/Umacodes.png';
+import umacodeslogo from './assets/images/logo.png';
+
+import { SocialIcon } from 'react-social-icons';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div className="text-div">
+
+          <div style={{background: 'white', borderRadius: '5px'}}>
+            <img src={umacodeslogo} className="umacodes-logo" alt="logo" />
+          </div>
+          <p>
+            I am Uma, a Software Engineer who loves to code. I work on cool programming projects and make videos explaining the process of making these projects.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>
+            Check out my Youtube Channel and other social media below.
+        </p>
+        </div>
+        <div style={{marginTop: '5px', marginBottom: '30px'}}>        
+          <a className="App-link" target="_blank" href="https://umaabu.com">Projects</a>
+        </div>
+        <div style={{ background: 'white', borderRadius: '5px' }}>
+          <SocialIcon target="_blank" url="https://www.youtube.com/channel/UCvDqylEzCZJHNea6k1XMkQw" network="youtube" style={{ margin: '5px' }} />
+          <SocialIcon target="_blank" url="https://www.instagram.com/umacodes/?igshid=7fb4ipoir4xc" network="instagram" bgColor="#000000" style={{ margin: '5px' }} />
+          <SocialIcon target="_blank" url="https://www.linkedin.com/in/umaabu/" network="linkedin" style={{ margin: '5px' }} />
+          <SocialIcon target="_blank" url="https://github.com/ueabu" network="github" bgColor="#000000" style={{ margin: '5px' }} />
+        </div>
+        <img src={umacodes} className="bubble-head" alt="logo" />
       </header>
     </div>
   );
